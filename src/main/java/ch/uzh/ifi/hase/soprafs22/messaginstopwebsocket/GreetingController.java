838f14fa806f8c1833808490df264a8f62f02052
arminveres <armin.veres@hotmail.com>
2022-04-22 20:24:51 +0200

    second version, changed path variables

--- src/main/java/ch/uzh/ifi/hase/soprafs22/messaginstopwebsocket/GreetingController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/messaginstopwebsocket/GreetingController.java
@@ -1,6 +1,7 @@
 package ch.uzh.ifi.hase.soprafs22.messaginstopwebsocket;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
@@ -13,7 +14,7 @@ public class GreetingController {
 
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
-    public Greeting greeting(HelloMessage message) throws Exception {
+    public Greeting greeting(@Payload HelloMessage message) throws Exception {
         Thread.sleep(1000);
         return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
--- src/main/java/ch/uzh/ifi/hase/soprafs22/messaginstopwebsocket/WebSocketConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/messaginstopwebsocket/WebSocketConfig.java
@@ -22,6 +22,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // registry.addEndpoint("/gs-guide-websocket");
-        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+        registry.addEndpoint("/websocket")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 }
